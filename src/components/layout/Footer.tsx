1bf038ca29742b871e2f1b1ffd5f3232369b621e
GeLoader <geloader1@gmail.com>
2025-05-15 00:51:09 +0800

    1

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
           <div>
             <div className="flex items-center mb-4">
               <img 
-                src="/vkweb1/vklogo.svg" 
+                src="/vkweb1/public/vklogo.svg" 
                 alt="Volove Kashrus Logo" 
                 className="h-8 w-8 mr-2"
               />
