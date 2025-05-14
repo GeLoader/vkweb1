584eb1a960a0f079282eeeca20fbf0ee6befdd26
GeLoader <geloader1@gmail.com>
2025-05-15 00:50:08 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/vkweb1",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
