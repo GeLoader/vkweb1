4d1344e40c664b181e20dde2eabc13920ce94182
Uppertech <jero@uppertech.net>
2025-06-19 21:49:14 +0000

    updated link

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -9,7 +9,7 @@ const Footer: React.FC = () => {
           <div>
             <div className="flex items-center mb-4">
               <img 
-                src="./vklogo.svg" 
+                src={`${import.meta.env.BASE_URL}/vklogo.svg`}
                 alt="Volove Kashrus Logo" 
                 className="h-8 w-8 mr-2"
               />
